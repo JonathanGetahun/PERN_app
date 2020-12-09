b906ca3eee9585513a2ea63f42aac4699206022c
Jonathan Getahun <kgetahun2@gmail.com>
2020-12-09 13:55:04 -0500

    added postgres db

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const db = require('./db')
+const morgan = require('morgan');
+
 const app = express();
 
+
+app.use(expres.json())
+app.use(morgan("tiny"))
+app.use((req, res, next) => {
+    console.log("it ran")
+    next()
+})
+
 //Get all restaurants
-    //using data{field} makes it easier for frontend to know where all the data 
-    //will be stored.
-   
-app.get("/api/v1/restaurants", (req, res) => {
+app.get("/api/v1/restaurants", async (req, res) => {
+    const results = await db.query("SELECT * FROM restaurants")
     res.status(200).json({
         status: "success",
         data: {
@@ -15,15 +24,42 @@ app.get("/api/v1/restaurants", (req, res) => {
     });
 });
 
-//Create a restaurant
-
-app.post("/api/v1/restaurants", (req, res) => {});
-
-
 //Get individual restaurant
 app.get("/api/v1/restaurants/:id", (req, res) => {
     console.log(req)
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "burgerking"
+        }
+    })
 });
+
+app.post("/api/v1/restaurants", (req, res) => {
+    console.log(req.body)
+    res.status(201).json({
+        status: "success",
+        data: {
+            restaurant: "burgerking"
+        }
+    })
+});
+
+
+app.put("/api/v1/restaurants/:id", (req,res) => {
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "burgerking"
+        }
+    })
+})
+
+app.delete("/api/v1/restaurants/:id", (req,res)=> {
+    res.status(204).json({
+        status: "success"
+    })
+})
 
 const port = process.env.PORT || 3001;
 app.listen(port , () => {
